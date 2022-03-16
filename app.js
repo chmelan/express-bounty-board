61e4bb28d7d1a01c2929505713ba5373beb5e4ea
Nick Chmela <nickchmela@gmail.com>
2022-03-16 16:10:59 -0400

    finalized development branch

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var compression = require("compression");
+var helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog");
 
 var app = express();
-
 //Set up mongoose connection
 var mongoose = require("mongoose");
 var mongoDB =
@@ -22,10 +23,14 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
